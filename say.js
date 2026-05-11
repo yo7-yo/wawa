c8e485503311d2e90166247cf0a870b87fbd23a6
yo7-yo <192969050+yo7-yo@users.noreply.github.com>
2026-05-12 03:26:14 +0800

    Update print statement from 'Hello' to 'Goodbye'注释

--- say.js
+++ say.js
@@ -232,7 +232,7 @@ function loadModel(modelPath) {
     loadingOverlay.style.display = 'block';
     const loader = new THREE.GLTFLoader();
 
-    loader.setMeshoptDecoder(MeshoptDecoder);
+    //loader.setMeshoptDecoder(MeshoptDecoder);
 
     if (model) {
         scene.remove(model);
